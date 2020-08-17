d53fea59884106555e6cf30c98b3ef5c8d88cdd6
Ailea Patrinely <aileapatrinely@gmail.com>
2020-08-17 09:31:42 -0500

    reinstates previously worked on chat code, server crashes due to socket.io code

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -3,6 +3,7 @@ import errors from './errors.reducer';
 import user from './user.reducer';
 import dog from './dogregister.reducer';
 import dogImage from './dogimage.reducer';
+import chat from './chat.reducer';
 
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
@@ -15,6 +16,7 @@ const rootReducer = combineReducers({
   user, // will have an id and username if someone is logged in
   dog,
   dogImage, //will add dog to user account
+  chat,
 });
 
 export default rootReducer;
