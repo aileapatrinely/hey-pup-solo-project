a65f59fb350ddcf35782655d77acf8e5cd7c6d68
Ailea Patrinely <aileapatrinely@gmail.com>
2020-09-24 11:01:13 -0500

    adding build directory for prod deployment

---
+++ build/precache-manifest.7ec18d10f5157b19e40804f79e3165fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0368ae554fc2ae15c52d6c1f59a9d3ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65de5a599750db5dd240",
+    "url": "/static/css/main.4925ab18.chunk.css"
+  },
+  {
+    "revision": "f5959fef677d283d2167",
+    "url": "/static/js/2.de0e2bb4.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/static/js/2.de0e2bb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65de5a599750db5dd240",
+    "url": "/static/js/main.2d8f7fc9.chunk.js"
+  },
+  {
+    "revision": "cfabed5267c41a13b59e",
+    "url": "/static/js/runtime-main.ddf8fe10.js"
+  }
+]);
